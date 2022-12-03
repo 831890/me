d3916fe08e24edf258cc84783fe4ce99435a1581
juahhhhhhhhhh <juahhhhhhhhhh@gmail.com>
2022-12-04 00:53:40 +0900

    INSTALL : react-rotating-text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,11 +5,17 @@ import Project from './Project'
 import styled from 'styled-components';
 
 export default function Main() {
+	const ReactRotatingText = require('react-rotating-text');
+
+	
 	return (
 		<>
 			<IntroCol>
 				<InnerCol>
-					<h2>안녕하세요? 김주아입니다.</h2>
+
+					<h2>안녕하세요? <strong>
+							<ReactRotatingText items={['Frontend', 'React', 'UI']} />
+						</strong>를 좋아하는 <ReactRotatingText items={['WREN', '김중앙', '보름달']} />입니다.</h2>
 				</InnerCol>
 			</IntroCol>
 			<About/>
@@ -35,3 +41,18 @@ const IntroCol = styled.div`
 		letter-spacing:4px;
 	}
 `
+// const ReactRotatingText = styled.span`
+//   animation: ${BlinkingCursor} 0.8s cubic-bezier(0.68, 0.01, 0.01, 0.99) 0s infinite;
+// `
+
+// const BlinkingCursor = styled.keyfram`
+// 0% {
+// 	opacity: 0;
+// }
+// 50% {
+// 	opacity: 1;
+// }
+// 100% {
+// 	opacity: 0;
+// }
+// `
